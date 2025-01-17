f4013194a6a7da93616f698222f5e3f428dfe67a
Trishiraj <gamerprince.exp@gmail.com>
2025-01-17 09:25:50 +0530

    Adjust window size for better list view

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import ui.MainScreen
 
 fun main() = application {
     val icon = painterResource("drawables/kmp-winget.png")
+    val state = rememberWindowState(
+        width = 800.dp,
+        height = 800.dp
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "kmp-winget",
         icon = icon,
-        resizable = false) {
+        resizable = false,
+        state = state
+    ) {
         MainScreen()
     }
 }
